a7f17e148bbf1272643ebe1e1726ffde10b5dda3
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-12-09 16:01:22 +0100

    Styling the buttons component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import Button from './components/Button'
+import { GoStar, GoSync } from "react-icons/go";
 
 const App = () => {
     return(
-        <div>
-            <div>
-                <Button>Plain</Button>
+        <div className='grid grid-cols-9 gap-1'>
+            <div className='grid grid-rows-6 grid-flow-col gap-2'>
+                <div>
+                    <Button>
+                        <GoStar className='mr-1' />
+                        Plain
+                    </Button>
+                </div>
+                <div>
+                    <Button primary>Primary</Button>
+                </div>
+                <div>
+                    <Button secondary>Secondary</Button>
+                </div>
+                <div>
+                    <Button success>Success</Button>
+                </div>
+                <div>
+                    <Button warning outlined>Warning</Button>
+                </div>
+                <div>
+                    <Button danger>Danger</Button>   
+                </div>
             </div>
-            <div>
-                <Button primary>Primary</Button>
+            <div className='grid grid-rows-6 grid-flow-col gap-2'>
+                <div>
+                    <Button rounded>Plain</Button>
+                </div>
+                <div>
+                    <Button primary rounded>Primary</Button>
+                </div>
+                <div>
+                    <Button secondary rounded>Secondary</Button>
+                </div>
+                <div>
+                    <Button success rounded>Success</Button>
+                </div>
+                <div>
+                    <Button warning outlined rounded>Warning</Button>
+                </div>
+                <div>
+                    <Button danger rounded>Danger</Button>   
+                </div>
             </div>
-            <div>
-                <Button secondary>Secondary</Button>
+            <div className='grid grid-rows-6 grid-flow-col gap-2'>
+                <div>
+                    <Button rounded outlined>Plain</Button>
+                </div>
+                <div>
+                    <Button primary rounded outlined>
+                        <GoSync className='mr-1'/>
+                        Primary
+                    </Button>
+                </div>
+                <div>
+                    <Button secondary rounded outlined>Secondary</Button>
+                </div>
+                <div>
+                    <Button success rounded outlined>Success</Button>
+                </div>
+                <div>
+                    <Button warning rounded outlined>Warning</Button>
+                </div>
+                <div>
+                    <Button danger rounded outlined>Danger</Button>   
+                </div>
             </div>
-            <div>
-                <Button success>Success</Button>
-            </div>
-            <div>
-                <Button warning outlined>Warning</Button>
-            </div>
-            <div>
-                <Button danger rounded>Danger</Button>   
+            <div className='grid grid-rows-6 grid-flow-col gap-2'>
+                <div>
+                    <Button outlined>Plain</Button>
+                </div>
+                <div>
+                    <Button primary outlined>Primary</Button>
+                </div>
+                <div>
+                    <Button secondary outlined>Secondary</Button>
+                </div>
+                <div>
+                    <Button success outlined>Success</Button>
+                </div>
+                <div>
+                    <Button warning outlined>Warning</Button>
+                </div>
+                <div>
+                    <Button danger outlined>Danger</Button>   
+                </div>
             </div>
         </div>
     )
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,7 @@
+import className from 'classnames'
+import { twMerge } from 'tailwind-merge';
+
+
 const Button = ({ 
     children,
     primary,
@@ -6,20 +10,29 @@ const Button = ({
     warning,
     danger,
     outlined,
-    rounded 
+    rounded,
+    ...rest
 }) => {
-    console.log('-------------------')
-    console.log('Primary is -->', primary)
-    console.log('Secondary is -->', secondary)
-    console.log('Success is -->', success)
-    console.log('Warning is -->', warning)
-    console.log('Danger is -->', danger)
-    console.log('Outlined is -->', outlined)
-    console.log('Rounded is -->', rounded)
+    const classes = twMerge(
+        className(rest.className, 'flex items-center gap-1 px-3 py-1.5 border', {
+          'border-blue-500 bg-blue-500 text-white': primary,
+          'border-gray-900 bg-gray-900 text-white': secondary,
+          'border-green-500 bg-green-500 text-white': success,
+          'border-yellow-400 bg-yellow-400 text-white': warning,
+          'border-red-500 bg-red-500 text-white': danger,
+          'rounded-full': rounded,
+          'bg-white': outlined,
+          'text-blue-500': outlined && primary,
+          'text-gray-900': outlined && secondary,
+          'text-green-500': outlined && success,
+          'text-yellow-400': outlined && warning,
+          'text-red-500': outlined && danger
+        })
+      );
 
     return(
         <div>
-            <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white"> { children } </button>
+            <button className={classes}> { children } </button>
         </div>
     )
 }
