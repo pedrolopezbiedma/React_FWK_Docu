16e998a0a376efe719b39e51c4f83810ae592d93
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-12-10 14:49:11 +0100

    Organizing the project better & creating the Accordion component

--- src/App.js
+++ src/App.js
@@ -1,94 +1,16 @@
-import Button from './components/Button'
-import { GoStar, GoSync } from "react-icons/go";
+import AccordionPage from "./pages/AccordionPage";
+import ButtonPage from "./pages/ButtonPage";
 
 const App = () => {
     return(
-        <div className='grid grid-cols-9 gap-1'>
-            <div className='grid grid-rows-6 grid-flow-col gap-2'>
-                <div>
-                    <Button>
-                        <GoStar className='mr-1' />
-                        Plain
-                    </Button>
-                </div>
-                <div>
-                    <Button primary>Primary</Button>
-                </div>
-                <div>
-                    <Button secondary>Secondary</Button>
-                </div>
-                <div>
-                    <Button success>Success</Button>
-                </div>
-                <div>
-                    <Button warning outlined>Warning</Button>
-                </div>
-                <div>
-                    <Button danger>Danger</Button>   
-                </div>
+        <div>
+            <div>
+                <h1>Below, Button page.</h1>
+                <ButtonPage />
             </div>
-            <div className='grid grid-rows-6 grid-flow-col gap-2'>
-                <div>
-                    <Button rounded>Plain</Button>
-                </div>
-                <div>
-                    <Button primary rounded>Primary</Button>
-                </div>
-                <div>
-                    <Button secondary rounded>Secondary</Button>
-                </div>
-                <div>
-                    <Button success rounded>Success</Button>
-                </div>
-                <div>
-                    <Button warning outlined rounded>Warning</Button>
-                </div>
-                <div>
-                    <Button danger rounded>Danger</Button>   
-                </div>
-            </div>
-            <div className='grid grid-rows-6 grid-flow-col gap-2'>
-                <div>
-                    <Button rounded outlined>Plain</Button>
-                </div>
-                <div>
-                    <Button primary rounded outlined>
-                        <GoSync className='mr-1'/>
-                        Primary
-                    </Button>
-                </div>
-                <div>
-                    <Button secondary rounded outlined>Secondary</Button>
-                </div>
-                <div>
-                    <Button success rounded outlined>Success</Button>
-                </div>
-                <div>
-                    <Button warning rounded outlined>Warning</Button>
-                </div>
-                <div>
-                    <Button danger rounded outlined>Danger</Button>   
-                </div>
-            </div>
-            <div className='grid grid-rows-6 grid-flow-col gap-2'>
-                <div>
-                    <Button outlined>Plain</Button>
-                </div>
-                <div>
-                    <Button primary outlined>Primary</Button>
-                </div>
-                <div>
-                    <Button secondary outlined>Secondary</Button>
-                </div>
-                <div>
-                    <Button success outlined>Success</Button>
-                </div>
-                <div>
-                    <Button warning outlined>Warning</Button>
-                </div>
-                <div>
-                    <Button danger outlined>Danger</Button>   
-                </div>
+            <div>
+                <h1>Below, Accordion page</h1>
+                <AccordionPage />
             </div>
         </div>
     )
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,7 @@
+const AccordionPage = () => {
+    return (
+        <h1>This is the Accordion page</h1>
+    )
+}
+
+export default AccordionPage
