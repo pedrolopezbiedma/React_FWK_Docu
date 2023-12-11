ef3784b2970fe4be2594e51ab1d1db602a3becc5
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-12-11 11:09:42 +0100

    Styling the selected path

--- src/components/navigation/Link.js
+++ src/components/navigation/Link.js
@@ -1,10 +1,10 @@
 import className from 'classnames'
 import useNavigationContext from '../../hooks/useNavigationContext';
 
-
-const Link = ({ children, pathTo, classNames }) => {
-    const { navigateTo } = useNavigationContext();
-    const finalClassName = className(classNames, 'text-blue-500');
+const Link = ({ children, pathTo, classNames, active }) => {
+    const { currentPath, navigateTo } = useNavigationContext();
+    const activeClassName = pathTo === currentPath ? 'font-bold border-l-4 border-blue-500 pl-2' : null
+    const finalClassName = className(classNames, 'text-blue-500 mb-3', activeClassName );
 
     const handleClick = (event) => {
         event.preventDefault();
