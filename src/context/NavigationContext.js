1e4e5a63a7fd6c66067ee9993aa9bd521754c76e
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-12-11 10:26:50 +0100

    Setting up Navigation Context

---
+++ src/context/NavigationContext.js
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useState } from "react";
+
+const NavigationContext = createContext();
+
+const NavigationContextProvider = ({ children }) => {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname)
+        }
+
+        window.addEventListener('popstate', handler)
+
+        // Clean up
+        return () => {
+            window.removeEventListener('popstate', handler)
+        }
+    }, [])
+
+    const navigateTo = (pathTo) => {
+        window.history.pushState({}, '', pathTo)
+    }
+
+    return (
+        <NavigationContext.Provider value={{ currentPath, navigateTo }}>
+            { children }
+        </NavigationContext.Provider>
+    )
+}
+
+export { NavigationContextProvider }
+export default NavigationContext;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 
 import App from './App';
+import { NavigationcontextProvider } from './context/NavigationContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <NavigationcontextProvider>
+        <App />
+    </NavigationcontextProvider>
 );
