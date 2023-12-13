2e211bc8ad49b2ace5e0485aefaeca52aa56c369
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-12-13 16:48:58 +0100

    Coding the reducer for the counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,75 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 
 import Panel from './Panel'
 import Button from './Button'
 
-const Counter = ({ initialCount }) => {
-    const [count, setCount] = useState(parseInt(initialCount));
-    const [valueToIncrement, setValueToIncrement] = useState('')
+// Initial state
+const initialState = {
+    count: 0,
+    valueToIncrement: ''
+}
+
+// Actions
+const INCREMENT_COUNT = 'increment_count'
+const DECREMENT_COUNT = 'decrement_count';
+const SET_VALUE_TO_INCREMENT = 'set_value_to_increment';
+const ADD_INCREMENT_TO_COUNT = 'add_increment_to_count';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+    
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+        
+        case SET_VALUE_TO_INCREMENT:
+            return {
+                ...state,
+                valueToIncrement: action.payload
+            }
+
+        case ADD_INCREMENT_TO_COUNT:
+            return {
+                ...state,
+                count: state.count + state.valueToIncrement,
+                valueToIncrement: 0
+            }
+
+        default:
+            return state;
+    }
+}
+
+const Counter = () => {
+    const [state, dispatch] = useReducer(reducer, initialState)
 
     const handleIncrement = () => {
-        setCount(count + 1)
+        dispatch({ type: INCREMENT_COUNT })
     }
 
     const handleDecrement = () => {
-        setCount(count - 1)
+        dispatch({ type: DECREMENT_COUNT })
     }
 
     const handleInputChange = (event) => {
-        setValueToIncrement(parseInt(event.target.value))
+        dispatch({ type: SET_VALUE_TO_INCREMENT, payload: parseInt(event.target.value) })
     }
 
     const handleAddToCount = (event) => {
         event.preventDefault();
-        setCount(count + valueToIncrement)
-        setValueToIncrement('')
+        dispatch({ type: ADD_INCREMENT_TO_COUNT })
     }
-    
+
     return (
         <Panel className='m-3'>
-            <h1 className='text-lg'>Count: { count }</h1>
+            <h1 className='text-lg'>Count: { state.count }</h1>
             <div className='flex flex-row'>
                 <Button primary onClick={handleIncrement}>Increment the count</Button>
                 <Button primary onClick={handleDecrement}>Decrement the count</Button>
@@ -35,7 +77,7 @@ const Counter = ({ initialCount }) => {
             <div>
                 <form onSubmit={handleAddToCount}>
                     <label htmlFor='count'>Number to add</label>
-                    <input className='p-1 m-3 bg-gray-50 border border-gray-300' id='count' type='number' value={valueToIncrement} onChange={(event) => handleInputChange(event)}/>
+                    <input className='p-1 m-3 bg-gray-50 border border-gray-300' id='count' type='number' value={ state.valueToIncrement } onChange={(event) => handleInputChange(event)}/>
                     <Button primary>Add the count</Button>
                 </form>
             </div>
--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,7 +2,7 @@ import Counter from "../components/Counter"
 
 const CounterPage = () => {
     return (
-        <Counter initialCount={0}/>
+        <Counter/>
     )
 }
 
