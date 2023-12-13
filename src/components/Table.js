ab5c63714b14afb1dd2c49e521886a820e6baf0d
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-12-13 10:07:29 +0100

    Building the table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,28 @@
 const Table = ({ items }) => {
     return(
-        <div>{ items.length }</div>
+        <div>
+            <div>{ items.length }</div>
+            <table className="table-auto border-spacing-2">
+                <thead>
+                    <tr className="border-b-2">
+                        <th>Name</th>
+                        <th>Color</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { items.map((item) => {
+                        return(
+                            <tr key={item.name} className="border-b">
+                               <td className="p-3">{ item.name }</td>
+                               <td className="p-3"><div className={`p-3 m-2 ${item.color}`}></div></td>
+                               <td className="p-3">{ item.score }</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,10 +1,10 @@
 import Table from "../components/Table"
 
 const items = [
-    { name: 'Apple', color: 'red', score: 3},
-    { name: 'Banana', color: 'yellow', score: 1},
-    { name: 'Lime', color: 'green', score: 5},
-    { name: 'Orange', color: 'orange', score: 2},
+    { name: 'Apple', color: 'bg-red-500', score: 3},
+    { name: 'Banana', color: 'bg-yellow-300', score: 1},
+    { name: 'Lime', color: 'bg-green-400', score: 5},
+    { name: 'Orange', color: 'bg-orange-400', score: 2},
 ]
 
 const TablePage = () => {
