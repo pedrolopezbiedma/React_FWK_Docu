033832059446ba22af1e68b09100e4a2b60784a7
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-04 09:26:11 +0100

    Some final changes

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,96 +1,113 @@
-import { useState } from "react"
+import { useState } from "react";
 import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
 
-import Table from "./Table"
+import Table from "./Table";
 
 const getSortValue = (item) => item.sortBy;
 
 const SortablePage = (props) => {
-    const { config, items } = props
-    const [sortBy, setSortBy] = useState(null);
-    const [sortOrder, setSortOrder] = useState(null);
-    console.log('order By is -->', sortBy ,' and sortOrder ', sortOrder)
+  const { config, items } = props;
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState(null);
+  console.log("order By is -->", sortBy, " and sortOrder ", sortOrder);
 
-    const getHeaderIcons = (column) => {
-        // TODO: Fix this!
-        if(sortBy){
-            if(sortBy !== column.label) {
-                return (
-                    <div>
-                        <GoTriangleUp />
-                        <GoTriangleDown />
-                    </div>
-                    )
-            } else {
-                if(sortOrder === 'asc') {
-                   return <GoTriangleDown />
-                }
+  const getHeaderIcons = (column) => {
+    if (column.label !== sortBy) {
+      return (
+        <div>
+          <GoTriangleUp />
+          <GoTriangleDown />
+        </div>
+      );
+    }
 
-            }
+    if (sortOrder === null) {
+      return (
+        <div>
+          <GoTriangleUp />
+          <GoTriangleDown />
+        </div>
+      );
+    } else if (sortOrder === "asc") {
+      return (
+        <div>
+          <GoTriangleUp />
+        </div>
+      );
+    } else if (sortOrder === "desc") {
+      return (
+        <div>
+          <GoTriangleDown />
+        </div>
+      );
+    }
+  };
+
+  const generateColumns = () => {
+    return config.map((configItem) => {
+      return {
+        ...configItem,
+        header: configItem.sortBy ? (
+          <th
+            className="cursor-pointer hover:bg-gray-100"
+            onClick={() => handleColumnClick(configItem.label)}
+          >
+            <div className="flex items-center">
+              {getHeaderIcons(configItem)}
+              {configItem.label}
+            </div>
+          </th>
+        ) : (
+          <th>{configItem.label}</th>
+        ),
+      };
+    });
+  };
+  const updatedConfig = generateColumns();
+
+  const generateRows = () => {
+    if (sortBy && sortOrder) {
+      const sortValue = getSortValue(
+        config.find((column) => column.label === sortBy)
+      );
+
+      return items.sort((a, b) => {
+        const valueA = sortValue(a);
+        const valueB = sortValue(b);
+
+        const orderValue = sortOrder === "asc" ? 1 : -1;
+
+        if (typeof valueA === "string") {
+          return valueA.localeCompare(valueB) * orderValue;
+        } else {
+          return (valueA - valueB) * orderValue;
         }
+      });
     }
 
-    const generateColumns = () => {
-        return config.map((configItem) => {
-            return {
-                ...configItem,
-                header: configItem.sortBy ? 
-                    <th className="cursor-pointer hover:bg-gray-100" onClick={() => handleColumnClick(configItem.label)}>
-                        <div className="flex items-center">
-                            {getHeaderIcons(configItem)}
-                            {configItem.label}
-                        </div>
-                    </th>
-                    :
-                    <th>{configItem.label}</th>
-            }
-        })
+    return props.items;
+  };
+  const updatedValues = generateRows();
+
+  const handleColumnClick = (clickedColumn) => {
+    setSortBy(clickedColumn);
+    switch (sortOrder) {
+      case null:
+        setSortOrder("asc");
+        break;
+      case "asc":
+        setSortOrder("desc");
+        break;
+      case "desc":
+        setSortOrder(null);
+        setSortBy(null);
+        break;
+      default:
+        break;
     }
-    const updatedConfig = generateColumns()
+  };
 
-    const generateRows = () => {
-        if(sortBy && sortOrder){
-            const sortValue = getSortValue(config.find((column) => column.label === sortBy))
+  return <Table {...props} config={updatedConfig} items={updatedValues} />;
+};
 
-            return items.sort((a, b,) => {
-                const valueA = sortValue(a);
-                const valueB = sortValue(b);
-        
-                const orderValue = sortOrder === 'asc' ? 1 : -1;
-        
-                if(typeof valueA === 'string'){
-                    return (valueA.localeCompare(valueB)) * orderValue
-                } else {
-                    return (valueA - valueB) * orderValue
-                }
-            })
-        }
-
-        return props.items
-    }
-    const updatedValues = generateRows()
-
-    const handleColumnClick = (clickedColumn) => {
-        setSortBy(clickedColumn)
-        switch (sortOrder) {
-            case null:
-                setSortOrder('asc');
-                break;
-            case 'asc':
-                setSortOrder('desc');
-                break;
-            case 'desc':
-                setSortOrder(null);
-                setSortBy(null)
-                break;
-            default:
-                break;
-        }
-    }
-
-    return(
-        <Table {...props} config={updatedConfig} items={updatedValues} />
-    )
-}
-
-export default SortablePage
+export default SortablePage;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,44 +1,65 @@
-import SortablePage from "../components/SortableTable"
-import Table from '../components/Table'
+import SortablePage from "../components/SortableTable";
 
-const items = [
-    { name: 'Apple', color: 'bg-red-500', score: 3},
-    { name: 'Banana', color: 'bg-yellow-300', score: 1},
-    { name: 'Lime', color: 'bg-green-400', score: 5},
-    { name: 'Orange', color: 'bg-orange-400', score: 2},
-]
+const sortableItems = [
+  { name: "Apple", color: "bg-red-500", score: 3 },
+  { name: "Banana", color: "bg-yellow-300", score: 1 },
+  { name: "Lime", color: "bg-green-400", score: 5 },
+  { name: "Orange", color: "bg-orange-400", score: 2 },
+];
+const sortableConfig = [
+  {
+    label: "Name",
+    render: (item) => item.name,
+    sortBy: (item) => item.name,
+  },
+  {
+    label: "Color",
+    render: (item) => <div className={`p-3 m-2 ${item.color}`}></div>,
+  },
+  {
+    label: "Score",
+    render: (item) => item.score,
+    sortBy: (item) => item.score,
+  },
+];
 
-const config = [
-    { 
-        label: 'Name',
-        render: (item) => item.name,
-        sortBy: (item) => item.name
-    },
-    { 
-        label: 'Color',
-        render: (item) => <div className={`p-3 m-2 ${item.color}`}></div>
-    },
-    { 
-        label: 'Score',
-        render: (item) => item.score,
-        sortBy: (item) => item.score,
-    },
-]
+const fixItems = [
+  { name: "Seat", brand: "Ibiza", value: 1500 },
+  { name: "Opel", brand: "Corsa", value: 2000 },
+  { name: "Nissan", brand: "Qashqai", value: 5000 },
+  { name: "Mini", brand: "Cooper", value: 2250 },
+];
+const fixConfig = [
+  {
+    label: "Car",
+    render: (item) => item.name,
+  },
+  {
+    label: "Brand",
+    render: (item) => item.brand,
+  },
+  {
+    label: "Cost",
+    render: (item) => item.value,
+  },
+];
 
-const keyFn = (item) => item.name
+const keyFn = (item) => item.name;
 
 const TablePage = () => {
-    return (
-        <div>
-            <h1>Sortable Table</h1>
-            <SortablePage items={items} config={config} keyFn={keyFn} />
+  return (
+    <div>
+      <p>Sortable Table</p>
+      <SortablePage
+        items={sortableItems}
+        config={sortableConfig}
+        keyFn={keyFn}
+      />
+      <br />
+      <p>Fix Table</p>
+      <SortablePage items={fixItems} config={fixConfig} keyFn={keyFn} />
+    </div>
+  );
+};
 
-            <br />
-            <h1>Fix Table</h1>
-            // TODO: Update info on this table
-            <Table items={ items } config={ config } keyFn = { keyFn }/>
-        </div>
-    )
-}
-
-export default TablePage
+export default TablePage;
